fc1d3e3b1cb18cb0264cd66529a9303f657bffac
Thomas B <69823294+VortexisTV@users.noreply.github.com>
2026-09-12 03:48:40 -0500

    Fix stencil sampling in attachment feedback loops

--- src/graphics/host_gpu/renderer/pipeline/descriptors.cpp
+++ src/graphics/host_gpu/renderer/pipeline/descriptors.cpp
@@ -995,15 +995,15 @@ void RenderExecutor::CommitBindings(CommandBuffer&                     buffer,
 					    std::ranges::find(image.views, binding.image_view, &CachedImageView::view);
 					EXIT_IF(storage || host_view == image.views.end());
 					const auto aspect = host_view->info.aspect;
-					const bool depth_feedback =
+					const bool attachment_feedback =
 					    layout == vk::ImageLayout::eAttachmentFeedbackLoopOptimalEXT &&
 					    program.stage == ShaderType::Pixel;
 					const bool depth_read =
-					    depth_feedback || layout == vk::ImageLayout::eDepthReadOnlyOptimal ||
+					    attachment_feedback || layout == vk::ImageLayout::eDepthReadOnlyOptimal ||
 					    layout == vk::ImageLayout::eDepthStencilReadOnlyOptimal ||
 					    layout == vk::ImageLayout::eDepthReadOnlyStencilAttachmentOptimal;
 					const bool stencil_read =
-					    layout == vk::ImageLayout::eStencilReadOnlyOptimal ||
+					    attachment_feedback || layout == vk::ImageLayout::eStencilReadOnlyOptimal ||
 					    layout == vk::ImageLayout::eDepthStencilReadOnlyOptimal ||
 					    layout == vk::ImageLayout::eDepthAttachmentStencilReadOnlyOptimal;
 					if ((aspect & vk::ImageAspectFlagBits::eDepth && !depth_read) ||
