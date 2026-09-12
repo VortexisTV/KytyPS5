49f53549fa10d5d2d0afc314d937c0a351d17ebc
Thomas B <69823294+VortexisTV@users.noreply.github.com>
2026-09-12 01:12:25 -0500

    Temp removal

--- src/graphics/shader/recompiler/ir/passes/ConstantPropagation.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "graphics/shader/recompiler/ir/Block.h"
-
-namespace Libs::Graphics::ShaderRecompiler::IR {
-
-void ConstantPropagationPass(const BlockList& blocks);
-
-} // namespace Libs::Graphics::ShaderRecompiler::IR
